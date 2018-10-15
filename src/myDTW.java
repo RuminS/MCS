10e3ddb1343ecb90e0cc12d6133d77d4b75d7865
Malala <randriamalal@gmail.com>
2018-10-15 11:51:56 +0200

    first commit

---
+++ src/myDTW.java
@@ -0,0 +1,14 @@
+import fr.enseeiht.danck.voice_analyzer.DTWHelper;
+import fr.enseeiht.danck.voice_analyzer.Field;
+
+public class myDTW extends DTWHelper {
+
+	@Override
+	public float DTWDistance(Field unknown, Field known) {
+		// Methode qui calcule le score de la DTW 
+		// entre 2 ensembles de MFCC
+		
+		return 0;
+	}
+
+}
---
+++ src/myMFCCdistance.java
@@ -0,0 +1,30 @@
+import fr.enseeiht.danck.voice_analyzer.MFCC;
+import fr.enseeiht.danck.voice_analyzer.MFCCHelper;
+
+public class myMFCCdistance extends MFCCHelper {
+
+	@Override
+	public float distance(MFCC mfcc1, MFCC mfcc2) {
+		// calcule la distance entre 2 MFCC
+		
+		return 0;
+	}
+
+	@Override
+	public float norm(MFCC mfcc) {
+		// retourne la valeur de mesure de la MFCC (coef d'indice 0 dans la MFCC) 
+		// cette mesure permet de determiner s'il s'agit d'un mot ou d'un silence
+		
+		return 0;
+	}
+
+	@Override
+	public MFCC unnoise(MFCC mfcc, MFCC noise) {
+		// supprime le bruit de la MFCC passee en parametre
+		// soustrait chaque coef du bruit a chaque coef du la MFCC 
+		// passee en parametre
+		
+		return null;
+	}
+
+}
