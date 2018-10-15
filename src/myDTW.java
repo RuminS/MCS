a73312f7ee022c03d63b1b5fa0ee1a62665db8dd
Malala <randriamalal@gmail.com>
2018-10-15 12:06:58 +0200

    computing score between two fields(dynamic prog)

--- src/myDTW.java
+++ src/myDTW.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.bcel.internal.generic.FLOAD;
+
 import fr.enseeiht.danck.voice_analyzer.DTWHelper;
 import fr.enseeiht.danck.voice_analyzer.Field;
 
@@ -7,8 +9,33 @@ public class myDTW extends DTWHelper {
 	public float DTWDistance(Field unknown, Field known) {
 		// Methode qui calcule le score de la DTW 
 		// entre 2 ensembles de MFCC
+		int n = unknown.getLength();
+		int m = known.getLength();
+		float left, up, upleft;
+		int w0, w1, w2;
+		w0 = 1; w1 = 2; w2 = 0;
+		float[][] result = new float[n][m]; // matrice d'alignement entre unknown et known
+		float d, score;
 		
-		return 0;
+		myMFCCdistance distanceCalculator = new myMFCCdistance();
+		
+		result[0][0] = 0;
+		for (int i = 1; i < m; i++) {
+			result[0][i] = Float.MAX_VALUE;
+		}
+		for (int i = 0 ; i < n ; i++) {
+			result[i][0] = Float.MAX_VALUE;
+			for (int j = 0 ; j < m ; j++) {
+				d = distanceCalculator.distance(unknown.getMFCC(i-1), 
+						known.getMFCC(j-1));
+				left = result[i][j-1];
+				upleft = result[i-1][j-1];
+				up = result[i-1][j];
+				result[i][j] = Float.min(left, Float.min(upleft, up));
+			}
+		}
+		score = result[n-1][m-1]/(unknown.getLength()+known.getLength());
+		return score;
 	}
 
 }
